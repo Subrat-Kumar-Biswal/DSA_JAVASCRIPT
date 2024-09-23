b9d272bdd5ff69c6bab382048b5c78f6b8043ee9
Subrat-Kumar-Biswal <subratkumar971@gmail.com>
2024-09-23 10:15:15 +0530

    add

--- ARRAY/Twosum.js
+++ ARRAY/Twosum.js
@@ -1,13 +1,12 @@
-var twoSum = function(nums, target) {
-    for(let i = 0; i<nums.length; i++){
-        for(let j =0; j<nums.length; j++){
-            if(nums[i]+nums[j] == target){
-                return "yes"
-            }
-        }
-        return "nO"
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return "yes";
+      }
     }
-    
+    return "no";
+  }
 };
 
-console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([2, 7, 11, 15], 9));
